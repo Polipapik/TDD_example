f9d47e0870b35695a0d725e5b7d406dd3f7ecb00
Polipapik <polipapik@gmail.com>
2018-12-13 14:59:14 +0300

    Hit func and tests added

--- cmd/character.go
+++ cmd/character.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 )
 
 //Character comment
 type Character struct {
-	Name string `json:"name"`
-	Hp   int    `json:"hp"`
-	Dmg  int    `json:"dmg"`
+	Name    string `json:"name"`
+	Hp      int    `json:"hp"`
+	Dmg     int    `json:"dmg"`
+	IsAlive bool   `json:"isalive"`
+}
+
+//Hit comment
+func (c Character) Hit(target *Character) string {
+	var ret string
+	if target.IsAlive == true {
+		target.Hp = target.Hp - c.Dmg
+		ret = fmt.Sprintf("%v hit %v, now %vs hp is %v", c.Name, target.Name, target.Name, target.Hp)
+		if target.Hp <= 0 {
+			target.IsAlive = false
+			ret = fmt.Sprintf("%v and %v died", ret, target.Name)
+		}
+	} else {
+		ret = fmt.Sprintf("%v is already dead", target.Name)
+	}
+	return ret
 }
 
 //JSONEncoder comment
@@ -18,6 +36,6 @@ func JSONEncoder(c Character) ([]byte, error) {
 
 //JSONDecoder comment
 func JSONDecoder(s []byte, c *Character) error {
-	//return json.Marshal(c)
-	return nil
+	return json.Unmarshal(s, &c)
+	//return nil
 }
--- cmd/character_test.go
+++ cmd/character_test.go
@@ -2,91 +2,35 @@ package main
 
 import (
 	"encoding/json"
-	"reflect"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var characters = map[string]Character{
+	"hero":    Character{Name: "Hero", Hp: 200, Dmg: 10, IsAlive: true},
+	"monster": Character{Name: "Monster", Hp: 25, Dmg: 5, IsAlive: true},
+	"boss":    Character{Name: "Boss", Hp: 50, Dmg: 20, IsAlive: true},
+}
+
 func TestJSONEncoder(t *testing.T) {
-	type args struct {
-		c Character
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    []byte
-		wantErr bool
-	}{
-		{
-			name: "test1",
-			args: args{c: Character{Name: "hero", Hp: 200, Dmg: 10}},
-			want: func() []byte {
-				ret, _ := json.Marshal(Character{Name: "hero", Hp: 200, Dmg: 10})
-				return ret
-			}(),
-			wantErr: false,
-		},
-		{
-			name: "test2",
-			args: args{c: Character{Name: "monster", Hp: 2, Dmg: 5}},
-			want: func() []byte {
-				ret, _ := json.Marshal(Character{Name: "monster", Hp: 2, Dmg: 5})
-				return ret
-			}(),
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := JSONEncoder(tt.args.c)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("JSONEncoder() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("JSONEncoder() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+	// 1 - инициализируем структуру, с помощью которой будем проверять
+	expected := characters["hero"]
 
-func TestJSONDecoder(t *testing.T) {
-	type args struct {
-		s []byte
-		c *Character
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := JSONDecoder(tt.args.s, tt.args.c); (err != nil) != tt.wantErr {
-				t.Errorf("JSONDecoder() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func TestJSONEncoders(t *testing.T) {
-	//1 - инициализируем структуру, с помощью которой будем проверять
-	expected := Character{Name: "hero", Hp: 200, Dmg: 10}
-
-	//2 - преобразовываем его в JSON формат
+	// 2 - преобразовываем его в JSON формат
 	encode, err := JSONEncoder(expected)
 	if err != nil {
 		t.Errorf("func JSONEncoder returned error: %s", err.Error())
 	}
 
-	//3 - проверяем преобразование через стандартные функции
+	// 3 - проверяем преобразование через стандартные функции
 	actual := Character{}
-	json.Unmarshal(encode, &actual)
+	if err = json.Unmarshal(encode, &actual); err != nil {
 
-	//4 - сравниваем
+	}
+
+	// 4 - сравниваем
 	if !assert.True(t, actual == expected) {
 		t.Errorf("func JSONEncoder returned unexpected result: got %v want %v", actual, expected)
 	}
@@ -95,7 +39,8 @@ func TestJSONEncoders(t *testing.T) {
 
 //TestJSONDecoder - тестируем обратное преобразование с JSON
 func TestJSONDecoders(t *testing.T) {
-	expected := Character{Name: "hero", Hp: 200, Dmg: 10}
+	// Выполняем действия обратные Encoding
+	expected := characters["hero"]
 
 	encode, _ := json.Marshal(expected)
 
@@ -107,4 +52,88 @@ func TestJSONDecoders(t *testing.T) {
 	if !assert.True(t, actual == expected) {
 		t.Errorf("func JSONDecoder returned unexpected result: got %v want %v", actual, expected)
 	}
+}
+
+//TestHitFunction - тестируем математику функции удара
+func TestHitFunction_CheckDamage(t *testing.T) {
+	hero := characters["hero"]
+	monster := characters["monster"]
+
+	expected := (monster.Hp - hero.Dmg)
+
+	hero.Hit(&monster)
+	actual := monster.Hp
+
+	if !assert.True(t, actual == expected) {
+		t.Errorf("func Hit returned unexpected monster hp result: got %vhp, want %vhp", actual, expected)
+	}
+	// Добавляем математику
+}
+
+//TestHitFunction - тестируем, умирает ли цель после удара
+func TestHitFunction_CheckIsAlive(t *testing.T) {
+	hero := characters["hero"]
+	monster := characters["monster"]
+
+	// Ожидаем, что монстр умрет
+	expected := false
+
+	hero.Hit(&monster)
+	hero.Hit(&monster)
+	hero.Hit(&monster)
+	actual := monster.IsAlive
+
+	if !assert.True(t, actual == expected) {
+		t.Errorf("func Hit returned unexpected monster isAlive result: got %v, want %v", actual, expected)
+	}
+	//В результате этого теста была добавлена проверка, если hp < 0, то персонаж умирает
+}
+
+//TestHitFunction - тестируем, удары по мертвым (лежачих не бьют)
+func TestHitFunction_CheckIsAliveHitting(t *testing.T) {
+	hero := characters["hero"]
+	monster := characters["monster"]
+
+	hero.Hit(&monster)
+	hero.Hit(&monster)
+	hero.Hit(&monster)
+	expected := monster.Hp // Записываем хп во время смерти
+
+	hero.Hit(&monster)
+	actual := monster.Hp
+
+	if !assert.True(t, actual == expected) {
+		t.Errorf("func Hit returned unexpected monster hp result: got %vhp, want %vhp", actual, expected)
+	}
+	// В результате этого теста в функции Hit появилась обертка в виде первого if target.IsAlive == true {..}
+}
+
+// TestHitFunction_Result - в нашей рпг должен быть хоть какой-то вывод. Эта функция проверяет выводы
+func TestHitFunction_Result(t *testing.T) {
+	hero := characters["hero"]
+	monster := characters["monster"]
+
+	expected := fmt.Sprintf("%v hit %v, now %vs hp is %v", hero.Name, monster.Name, monster.Name, (monster.Hp - hero.Dmg))
+	actual := hero.Hit(&monster)
+
+	if !assert.True(t, actual == expected) {
+		t.Errorf("func Hit returned unexpected result: got { %v }, want { %v }", actual, expected)
+	}
+
+	hero.Hit(&monster)
+
+	expected = fmt.Sprintf("%v hit %v, now %vs hp is %v and %v died", hero.Name, monster.Name, monster.Name, (monster.Hp - hero.Dmg), monster.Name)
+	actual = hero.Hit(&monster)
+
+	if !assert.True(t, actual == expected) {
+		t.Errorf("func Hit returned unexpected result: got { %v }, want { %v }", actual, expected)
+	}
+
+	expected = fmt.Sprintf("%v is already dead", monster.Name)
+	actual = hero.Hit(&monster)
+
+	if !assert.True(t, actual == expected) {
+		t.Errorf("func Hit returned unexpected result: got { %v }, want { %v }", actual, expected)
+	}
+	// В результате этого теста добавилось возвращаемое значение функции Hit, и логика связанная с ним
 }
